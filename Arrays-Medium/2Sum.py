0c3441401ec2ec3426559f3f3d53b05aef34c7f9
in_jatin <in_jatin@m1.com.sg>
2024-10-22 21:34:48 +0530

    revisited 22-oct-2024

--- Arrays-Medium/2Sum.py
+++ Arrays-Medium/2Sum.py
@@ -1,17 +1,76 @@
-def read(n: int, book: [int], target: int) -> str:
-	d = {}
-	for i in range(n):
-		if book[i] not in d:
-			d[book[i]] = 1
+# def read(n: int, book: [int], target: int) -> str:
+# 	d = {}
+# 	for i in range(n):
+# 		if book[i] not in d:
+# 			d[book[i]] = 1
+# 		else:
+# 			d[book[i]] += 1
+
+# 		if target - book[i] == 0:
+# 			return "No"
+
+# 		if (target - book[i]) in d:
+# 			return "YES"
+# 	else:
+# 		return "NO"
+
+# print(read(2,[1,2],1))
+
+
+
+# 2 pointer optimal solution -  can only work on sorted array
+'''
+def twoPointer(arr,target):
+	arr = sorted(arr)
+
+	left = 0
+	right = len(arr)-1
+	s = 0
+
+	while left < right:
+		s = arr[left] + arr[right]
+
+		print(s,left,right)
+
+		if s > target:
+			right-=1
+		elif s < target:
+			left+=1
 		else:
-			d[book[i]] += 1
+			return left, right
+	return -1,-1
+		
 
-		if target - book[i] == 0:
-			return "No"
+# print(twoPointer([2,6,5,8,11],11))
 
-		if (target - book[i]) in d:
-			return "YES"
+print(twoPointer([3,2,4],6))
+'''
+
+
+
+
+# dictionary approach to return indicies
+
+def dicApproachToReturnIndicies(arr,target):
+    
+	d = {}
+
+	'''
+		a+b = target
+		b = target-a
+	'''
+
+	for i in range(len(arr)):
+		if arr[i] not in d:
+			d[arr[i]] = i
+		
+	for j in range(len(arr)):
+		if target-arr[j] in d and j != d[target-arr[j]]:
+			return [j,d[target-arr[j]]]
 	else:
-		return "NO"
+		return [-1,-1]
+	
 
-print(read(2,[1,2],1))
+# print(dicApproachToReturnIndicies([3,2,4],6))
+
+print(dicApproachToReturnIndicies([2,6,5,8,10],13))
--- Arrays-Medium/Sort012.py
+++ Arrays-Medium/Sort012.py
@@ -4,23 +4,67 @@ from collections import *
 from math import *
 
 
-def sortArray(arr, n):
-	# Write your code here
-	low, mid = 0, 0
-	high = n - 1
+# def sortArray(arr, n):
+# 	# Write your code here
+# 	low, mid = 0, 0
+# 	high = n - 1
+
+# 	while mid <= high:
+# 		if arr[mid] == 0:
+# 			arr[low], arr[mid] = arr[mid], arr[low]
+# 			low += 1
+# 			mid += 1
+# 		elif arr[mid] == 1:
+# 			mid += 1
+# 		else:
+# 			arr[mid], arr[high] = arr[high], arr[mid]
+# 			high -= 1
+
+# 	return arr
+
+
+# print(sortArray([2, 0, 2, 1, 1, 0], 6))
+
+
+
+
+# we will use DNF(Dutch National Flag ) algo
+'''
+use 3 pointers - low, mid , high
+
+0 ... low-1 -> extreme left (sorted) contains 0
+low ... mid-1 -> contains 1
+high+1 ... n-1 -> extreme right contains 2
+
+we know from 0 ... mid-1 is sorted
+and from high+1 ... n-1 is sorted
+
+And we infer [mid,high] is unsorted
+Initially we will start with entire array which is unsorted, so mid = 0 high = n-1
+'''
+
+def dnf(arr):
+	mid = 0
+	high = len(arr)-1
+	low = 0
 
 	while mid <= high:
 		if arr[mid] == 0:
-			arr[low], arr[mid] = arr[mid], arr[low]
-			low += 1
-			mid += 1
+			arr[low],arr[mid] = arr[mid],arr[low]
+			low+=1
+			mid+=1
 		elif arr[mid] == 1:
-			mid += 1
+			mid+=1
+		
 		else:
-			arr[mid], arr[high] = arr[high], arr[mid]
-			high -= 1
+			arr[mid],arr[high] = arr[high],arr[mid]
+			high-=1
 
+	
 	return arr
 
+print(dnf([2,0,1,2,1,1,0,1,0]))
 
-print(sortArray([2, 0, 2, 1, 1, 0], 6))
+print(dnf([2,0,0,0,0]))
+print(dnf([2,1,1,1,1]))
+print(dnf([2,2,2,2,0]))
