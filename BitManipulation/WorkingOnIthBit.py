fcc80330105abf15596d0ae653b37ca1bed50297
jatin.batish <jatinbatish098@gmail.com>
2025-05-28 10:19:32 +0530

    -> CheckIfPowerOf2()

--- BitManipulation/WorkingOnIthBit.py
+++ BitManipulation/WorkingOnIthBit.py
@@ -13,3 +13,12 @@ print(clearIthBit(13,2))
 def toggleIthBit(n,i):
     return n ^ (1<<i)
 print(toggleIthBit(13,2))
+
+
+def checkIfPowerOf2(n):
+    if n == 0:
+        return False
+    if n & n - 1 == 0:
+        return True
+    return False
+print(checkIfPowerOf2(16))
